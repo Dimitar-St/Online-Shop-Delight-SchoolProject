675de022684bb7646ac81a51039943ea297fc70d
Dimitar <stoyanov.dimi.tri@gmail.com>
2017-11-08 00:34:40 +0100

    I removed the home routes class and make a function.

--- routers/home-routes.js
+++ routers/home-routes.js
@@ -1,27 +1,8 @@
-class HomeRoutes {
-    constructor(router, controller) 
-    {
-        this.router = router;
-        this.controller = controller;
-    }
-    
-    get router() {
-        return this._router;
-    }
-    
-    set router(value) {
-        if(value === null || value === undefined) {
-           throw 'The passed router is invalid.';
-        }
-        
-        this._router = value;
-    }
-    
-    loadRoutes() {
-        this.router
-            .get('/', (req, res) => this.controller.loadHomePage(req, res))
-            .get('/for-us', (req, res) => this.controller.loadForUsPage(req, res));
-    }
-}
+const HomeController = require('../controllers/home-controller.js');
 
-module.exports = HomeRoutes;
+module.exports = function(router) {
+      let controller = new HomeController();
+    
+      router.get('/', (req, res) => controller.loadHomePage(req, res))
+            .get('/for-us', (req, res) => controller.loadForUsPage(req, res));
+};
--- routers/routes-loader.js
+++ routers/routes-loader.js
@@ -1,6 +1,3 @@
-const HomeRoutes = require('./home-routes.js'),
-      HomeController = require('../controllers/home-controller.js');
-
 class AllRoutes {
     constructor(app, router) {
         this.app = app;
@@ -8,16 +5,12 @@ class AllRoutes {
     }
     
     load() {
-        let homeController = new HomeController();
-        
-        let homeRoute = new HomeRoutes(this.router, homeController);
         
         require('./authentication-routes.js')(this.router);
         require('./user-routes.js')(this.router);
         require('./admin-routes.js')(this.router);
         require('./products-routes.js')(this.router);
-        
-        homeRoute.loadRoutes();
+        require('./home-routes.js')(this.router);
         
         this.app.use(this.router);
     }
