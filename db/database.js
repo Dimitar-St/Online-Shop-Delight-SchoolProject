4f6ed6a61b44c32397142746c8e6b1c7c648b6b7
Dimitar <stoyanov.dimi.tri@gmail.com>
2018-01-17 13:00:15 +0200

    I added the heroku database.

--- db/database.js
+++ db/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 module.exports = () => {
-  mongoose.connect('mongodb://localhost:27017/Shop-Delight', {
+  mongoose.connect('mongodb://<DimitarStoynaov>:<3343568mLab>@ds259897.mlab.com:59897/naslada', {
     useMongoClient: true,
   });   
 }
