e850a8c4329fabbbff736039b3b779d843693cbd
Dimitar <stoyanov.dimi.tri@gmail.com>
2018-01-17 13:08:04 +0200

    Changed db connection.

--- db/database.js
+++ db/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 module.exports = () => {
-  mongoose.connect('mongodb://<DimitarStoynaov>:<3343568mLab>@ds259897.mlab.com:59897/naslada', {
+  mongoose.connect('mongodb://DimitarStoynaov:3343568mLab@ds259897.mlab.com:59897/naslada', {
     useMongoClient: true,
   });   
 }
