1b33c85da0f74e429db96a9a4cfb80809e71510d
Dimitar <stoyanov.dimi.tri@gmail.com>
2018-01-17 13:37:31 +0200

    I added promise catching.

--- db/database.js
+++ db/database.js
@@ -4,5 +4,11 @@ mongoose.Promise = global.Promise;
 module.exports = () => {
   mongoose.connect(process.env.MONGODB_URI, {
     useMongoClient: true,
+  }, function (err, res) {
+    if (err) {
+    console.log ('ERROR connecting to: ' + process.env.MONGODB_URI + '. ' + err);
+    } else {
+    console.log ('Succeeded connected to: ' + process.env.MONGODB_URI);
+    }
   });   
 }
